383ec9857b63590326b39c1e3ffac24e34512bb2
Денис Котелев <deniskotelev@MacBook-Pro-Denis.local>
2023-10-29 22:00:40 +0300

    feat

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import 'assets/styles/normalize.css';
 import type { AppProps } from 'next/app';
+import { Layout } from '@components/index';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Component {...pageProps} />
+    <Layout
+      title="LEPRO MIXER - BEST CRYPTO MIXER"
+      description="LEPRO MIXER - BEST CRYPTO MIXER"
+      keywords="LEPRO, CRYPTO MIXER, BETST BITCOIN MIXER"
+    >
+      <Component {...pageProps} />
+    </Layout>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
-import {
-  Layout,
-} from '@components/index';
 
 function Page() {
   return (
-    <Layout
-      title="LEPRO MIXER - BEST CRYPTO MIXER"
-      description="LEPRO MIXER - BEST CRYPTO MIXER"
-      keywords="LEPRO, CRYPTO MIXER, BETST BITCOIN MIXER"
-    >
-      App
-    </Layout>
+    <div>
+      12312312
+    </div>
   );
 }
 
